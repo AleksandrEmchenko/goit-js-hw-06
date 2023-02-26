8a84a0bbb727417a57e3aceee6aa3b55f3b8ff51
Aleksandr Yemchenko <aleksandremchenko@gmail.com>
2023-02-26 13:20:03 +0200

    fix task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,17 +1,17 @@
-const form = document.querySelector('.login-form');
+const form = document.querySelector(".login-form");
 
-form.addEventListener('submit', handleFormControl);
-const {email, password} = form;
+form.addEventListener("submit", handleFormControl);
+const { email, password } = form;
 
-function handleFormControl (event) {
-    event.preventDefault();
+function handleFormControl(event) {
+  event.preventDefault();
 
-    if (email.value === "" || password.value === "") {
-    alert ("Please fill in all the fields!");
+  if (email.value === "" || password.value === "") {
+    alert("Please fill in all the fields!");
   }
 
-    if(email.value && password.value) {
-      console.log(`{ ${email.value}; ${password.value} }`);
-      event.currentTarget.reset();
-    }
+  if (email.value && password.value) {
+    console.log({ email: email.value, password: password.value });
+    event.currentTarget.reset();
+  }
 }
